c7e4ad02d6c13c4bad36b37e70208b4c337568d5
Stephen Frey <Sfreyjr@gmail.com>
2024-05-14 21:53:29 -0400

    Further work done on getting cookies to work, now starting an initial check

--- ProsperBrowse.py
+++ ProsperBrowse.py
@@ -1,9 +1,19 @@
-class interCookie:
+def interCookie(mode):
+    #0 doesn't return a value, 1 does.
     with open('cookies.txt', 'r') as fileContent:
         contents = fileContent.read()
         #print(contents)
         #this works thus far
+        if mode == 0:
+            return contents
+        print(contents)
         
-class setUp
-        
-interCookie()
+class setUp:
+   def initx2():
+       comp = interCookie(0)
+ 
+runStart = setUp
+interCookie(1)
+print("ReadCookies")
+runStart.initx2
+
